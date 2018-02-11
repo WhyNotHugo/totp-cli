5d9baaba2522e0691e510e181e640f53fb058059
Eduardo Dobay <edudobay@gmail.com>
2018-02-11 12:00:08 -0200

    A possibly cleaner implementation of CLI parsing
    Grabbed suggestions from [1]
    
    [1] https://mike.depalatis.net/blog/simplifying-argparse.html

--- totp/cli.py
+++ totp/cli.py
@@ -1,34 +1,20 @@
 import argparse
 import sys
-from collections import OrderedDict, defaultdict
+from collections import OrderedDict, defaultdict, namedtuple
 from functools import reduce
 
 import totp
 
-# Suggestion from https://stackoverflow.com/a/26379693/302264, refactored
-def _ensure_default_subparser(default, parser, args):
-    """Ensure a subcommand is present in `args`; if no valid subcommand is
-    found, `default` is inserted as the first element of `args`.
-    default: the name of the subparser to call by default
-    args: the argument list that is to be handed to parse_args()
-    """
-    def add_default_subcommand(args, default):
-        # insert default in first position, this implies no
-        # global options without a sub_parsers specified
-        args.insert(0, default)
+_subcommands = {}
 
-    def has_explicit_subcommand(args, parser):
-        return any(sp_name in args
-            for action in parser._subparsers._actions
-                   if isinstance(action, argparse._SubParsersAction)
-            for sp_name in action._name_parser_map.keys())
+_argument = namedtuple('argument', ['args', 'kwargs'])
+def argument(*args, **kwargs):
+    return _argument(args, kwargs)
 
-    for arg in args:
-        if arg in ['-h', '--help']:  # global help if no subparser
-            return
-
-    if not has_explicit_subcommand(args, parser):
-        add_default_subcommand(args, default=default)
+def subcommand(name, *args, **kwargs):
+    def decorator(func):
+        _subcommands[name] = (args, kwargs, func)
+    return decorator
 
 def _parse_args(args):
     parser = argparse.ArgumentParser(
@@ -37,43 +23,44 @@ def _parse_args(args):
 
     subparsers = parser.add_subparsers(dest='command')
 
-    # ---
-    add_parser = subparsers.add_parser('add',
-        description='Add a new TOTP entry to the database.',
-        help='add a new TOTP entry to the database')
+    for name, (_args, kwargs, func) in _subcommands.items():
+        _parser = subparsers.add_parser(name, **kwargs)
+        _parser.set_defaults(func=func)
+        for arg in _args:
+            _parser.add_argument(*arg.args, **arg.kwargs)
 
-    add_parser.add_argument('identifier',
-        help='the identifier under the \'2fa\' folder where the key should be saved')
+    def add_default_subcommand_if_omitted(args, default):
+        if not any(arg in ('-h', '--help') or arg in subparsers.choices for arg in args):
+            args.insert(0, default)
 
-    # ---
-    show_parser = subparsers.add_parser('show',
-        description='Show the current TOTP token for a registered entry.',
-        help='(default action) show the current TOTP token for a registered entry')
+    add_default_subcommand_if_omitted(args, 'show')
 
-    show_parser.add_argument('-s', dest='offset_seconds', metavar='SECONDS', default=0,
-        help='offset the clock by the given number of seconds')
-
-    show_parser.add_argument('identifier',
-        help='the identifier by which the key can be found under the \'2fa\' folder')
-
-    # ---
-    _ensure_default_subparser('show', parser, args)
     return parser.parse_args(args)
 
 def run():
     args = _parse_args(sys.argv[1:])
     try:
-        _run(args)
+        args.func(args)
     except KeyboardInterrupt:
         print()
 
-def _run(args):
-    if args.command == 'add':
-        totp.add_pass_entry(args.identifier)
-    elif args.command == 'show':
-        totp.generate_token(args.identifier, seconds=args.offset_seconds)
-    else:
-        assert False, 'unexpected command: %r' % args.command
+@subcommand('add',
+    argument('identifier',
+        help='the identifier under the \'2fa\' folder where the key should be saved'),
+    description='Add a new TOTP entry to the database.',
+    help='add a new TOTP entry to the database')
+def _cmd_add(args):
+    totp.add_pass_entry(args.identifier)
+
+@subcommand('show',
+    argument('-s', dest='offset_seconds', metavar='SECONDS', default=0,
+        help='offset the clock by the given number of seconds'),
+    argument('identifier',
+        help='the identifier by which the key can be found under the \'2fa\' folder'),
+    description='Show the current TOTP token for a registered entry.',
+    help='(default action) show the current TOTP token for a registered entry')
+def _cmd_show(args):
+    totp.generate_token(args.identifier, seconds=args.offset_seconds)
 
 if __name__ == '__main__':
     run()
